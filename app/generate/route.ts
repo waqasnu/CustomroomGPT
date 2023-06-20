275a911f7ff6664e2f182f94f2c91bc2ae6830b0
waqasnu <waqascs@gmail.com>
2023-06-20 21:14:49 +0500

    Update route.ts

--- app/generate/route.ts
+++ app/generate/route.ts
@@ -38,7 +38,7 @@ export async function POST(request: Request) {
 
 
    //const wallcolor = "Red";
-   const aprompt = "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning," + wall.toLowerCase();
+   const aprompt = "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning, " + wall.toLowerCase();
 
   // POST request to Replicate to start the image restoration generation process
   let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
