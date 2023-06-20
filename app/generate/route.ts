81b830761495aa76729d40cb2a276673950e6fcd
waqasnu <waqascs@gmail.com>
2023-06-20 05:11:47 +0500

    Update route.ts

--- app/generate/route.ts
+++ app/generate/route.ts
@@ -37,7 +37,7 @@ export async function POST(request: Request) {
   const { imageUrl, theme, room } = await request.json();
 
   
- //  const aprompt = "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning";
+   const aprompt = "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning, wall color blue";
 
   // POST request to Replicate to start the image restoration generation process
   let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
@@ -56,7 +56,7 @@ export async function POST(request: Request) {
             ? "a room for gaming with gaming computers, gaming consoles, and gaming chairs"
             : `a ${theme.toLowerCase()} ${room.toLowerCase()}`,
         a_prompt:
-          "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning, `${room}` " ,
+          `${aprompt}`,
         n_prompt:
           "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
       },
